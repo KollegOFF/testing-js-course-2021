09893dd117a25dcce1ce9ad78189c1c26f7ca778
Evgeniy Khaberev <ehaberev@yandex.ru>
2021-04-18 00:56:09 +0500

    Add integration test example

--- 4-Integration-Testing/isolation-demo/tests/todo-list.test.js
+++ 4-Integration-Testing/isolation-demo/tests/todo-list.test.js
@@ -1,5 +1,6 @@
 import TodoItem from "../src/todo-item";
 import TodoList from "../src/todo-list";
+import DataService from '../src/data-service';
 
 describe('Todo List tests', () => {
 
@@ -7,19 +8,12 @@ describe('Todo List tests', () => {
     // Arrange
     expect.hasAssertions();
 
-    const items = ['Test item 1', 'Test item 2'];
+    const items = ['Сварить борщ', 'Сходить в тренажерный зал'];
     const expectedItems = items.map((item) => {
       return new TodoItem(item);
     });
 
-    const dataService = {
-      // async load() {
-      //   return Promise.resolve(expectedItems);
-      // }
-    };
-
-    dataService.load = jest.fn().mockResolvedValue(expectedItems);
-
+    const dataService = new DataService('http://localhost:3000', 'todo-items');
     const todoList = new TodoList(dataService);
 
     //Act
@@ -28,53 +22,5 @@ describe('Todo List tests', () => {
     //Assert
     expect(todoList.items).toEqual(expectedItems);
 
-  });
-
-  test('Save items sucessfully', () => {
-    // Arrange
-    expect.assertions(5);
-
-    const sourceItems = [
-      {
-        name: 'Test item 1',
-        isDone: false
-      },
-      {
-        name: 'Test item 2',
-        isDone: true
-      },
-      {
-        name: 'Test item 3',
-        isDone: false
-      }
-    ];
-
-    const expectedItems = sourceItems.filter((item) => !item.isDone);
-
-    let savedData = [];
-
-    const dataService = {
-      // save(item) {
-      //   savedData.push(item);
-      // }
-    };
-
-    dataService.save = jest.fn();
-
-    const todoList = new TodoList(dataService);
-    todoList.items = JSON.parse(JSON.stringify(sourceItems));
-
-    const expectedTimesToCall = 2;
-
-    //Act
-    todoList.save();
-
-    //Assert
-    expect(dataService.save).toHaveBeenCalledTimes(expectedTimesToCall);
-
-    for (let i = 0; i < expectedTimesToCall; i++) {
-      expect(dataService.save.mock.calls[i][0]).toEqual(expectedItems[i]);
-      expect(dataService.save.mock.calls[i].length).toBe(1);
-    }
   });
 });
